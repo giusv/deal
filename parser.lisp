8684405f8586c8bd3de9bb9b82e9e6661775b863
giusv <gius.viola@gmail.com>
2017-01-24 16:41:28 +0100

    ok parser choose

--- parser.lisp
+++ parser.lisp
@@ -3,15 +3,66 @@
   (snd nil))
 (defun tuple (fst snd)
   (make-tuple :fst fst :snd snd)) 
-(defun result (v)
-  #'(lambda (inp) (tuple v inp)))
+(defun result (x)
+  #'(lambda (s) 
+      (list (tuple x s))))
 
-(defun zero ()
-  #'(lambda () ()))
+(defun bind (p q)
+  #'(lambda (s)
+      (apply #'concatenate 
+	     'list
+	     (mapcar #'(lambda (tuple)
+			 (let ((x (tuple-fst tuple))
+			       (s1 (tuple-snd tuple)))
+			   (apply-parser (funcall q x) s1)))
+		     (apply-parser p s)))))
+
+
+(defun fail ()
+  #'(lambda (*) ()))
   
 (defun item ()
-  #'(lambda (inp) (cond ((null inp) ())
-			(t (list (tuple (car inp) (cdr inp)))))))
+  #'(lambda (s) (cond ((null s) ())
+			(t (list (tuple (car s) (cdr s)))))))
 
+(defun apply-parser (parser input)
+  (funcall parser input))
 (defun parse (parser input)
-  (funcall (funcall parser) input))
+  (let ((res (car (apply-parser parser input)))) 
+    (if (null res) 
+	nil
+	(tuple-fst res))))
+
+
+(defmacro doo (&rest binds)
+  (if (eq (length binds) 1) 
+      (car binds)
+      (case (length (car binds))
+	(1 `(bind ,(cadar binds) #'(lambda (*) ,(apply #'dooo (cdr binds)))))
+	(2 `(bind ,(cadar binds) #'(lambda (,(caar binds)) ,(apply  #'dooo (cdr binds))))))))
+
+(defun dooo (&rest binds)
+  (if (eq (length binds) 1) 
+      (car binds)
+      (case (length (car binds))
+	(1 `(bind ,(cadar binds) #'(lambda (*) ,(apply #'dooo (cdr binds)))))
+	(2 `(bind ,(cadar binds) #'(lambda (,(caar binds)) ,(apply  #'dooo (cdr binds))))))))
+
+
+(defun sat (p)
+  (doo (s (item))
+       (if (funcall p s)
+ 	   (result s)
+	   (fail))))
+;;; (parse (bind (item) #'(lambda (a) (bind (item) #'(lambda (*) (result (list (incf a))))))) '(1 2))
+
+(defun sym (m)
+  (doo (a (sat #'(lambda (x) (eq m x))))
+       (result a)))
+
+(defun choose (p q)
+  #'(lambda (s)
+      (let ((ps (apply-parser p s)))
+	(if (null ps)
+	    (apply-parser q s)
+	    ps))))
