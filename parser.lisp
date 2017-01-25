f88aa2e544e42aa9c8e7b8c69b78adadfe7e5730
Giuseppe Viola <gius.viola@gmail.com>
2017-01-25 06:35:47 +0100

    ok choose many sepby

--- parser.lisp
+++ parser.lisp
@@ -7,6 +7,15 @@
   #'(lambda (s) 
       (list (tuple x s))))
 
+(defun apply-parser (parser input)
+  (funcall parser input))
+(defun parse (parser input)
+  (let ((res (car (apply-parser parser input)))) 
+    (if (null res) 
+	nil
+	(tuple-fst res))))
+
+
 (defun bind (p q)
   #'(lambda (s)
       (apply #'concatenate 
@@ -25,21 +34,6 @@
   #'(lambda (s) (cond ((null s) ())
 			(t (list (tuple (car s) (cdr s)))))))
 
-(defun apply-parser (parser input)
-  (funcall parser input))
-(defun parse (parser input)
-  (let ((res (car (apply-parser parser input)))) 
-    (if (null res) 
-	nil
-	(tuple-fst res))))
-
-
-(defmacro doo (&rest binds)
-  (if (eq (length binds) 1) 
-      (car binds)
-      (case (length (car binds))
-	(1 `(bind ,(cadar binds) #'(lambda (*) ,(apply #'dooo (cdr binds)))))
-	(2 `(bind ,(cadar binds) #'(lambda (,(caar binds)) ,(apply  #'dooo (cdr binds))))))))
 
 (defun dooo (&rest binds)
   (if (eq (length binds) 1) 
@@ -47,6 +41,15 @@
       (case (length (car binds))
 	(1 `(bind ,(cadar binds) #'(lambda (*) ,(apply #'dooo (cdr binds)))))
 	(2 `(bind ,(cadar binds) #'(lambda (,(caar binds)) ,(apply  #'dooo (cdr binds))))))))
+
+(defmacro doo (&rest binds)
+  (if (eq (length binds) 1) 
+      (car binds)
+      (case (length (car binds))
+	(1 `(bind ,(cadar binds) #'(lambda (*) ,(apply #'dooo (cdr binds)))))
+	(2 `(bind ,(cadar binds) #'(lambda (,(caar binds)) ,(apply #'dooo (cdr binds))))))))
+
+
 
 
 (defun sat (p)
@@ -66,3 +69,31 @@
 	(if (null ps)
 	    (apply-parser q s)
 	    ps))))
+
+(defun zero ()
+  #'(lambda (*) ()))
+(defun plus (p q)
+  #'(lambda (s)
+      (append (apply-parser p s) (apply-parser q s))))
+(defun choice (p q)
+  #'(lambda (s)
+      (let ((res (apply-parser (plus p q) s)))
+	(if (null res)
+	    nil
+	    (car res)))))
+
+(defun many (p)
+  (choose (many1 p)
+	  (result nil)))
+(defun many1 (p)
+  (doo (a p)
+       (as (many p))
+       (result (cons a as))))
+(defun sepby (p sep)
+  (choose (sepby1 p sep)
+	  (result nil)))
+(defun sepby1 (p sep)
+  (doo (a p)
+       (as (many (doo (* sep)
+		      p)))
+       (result (cons a as))))
