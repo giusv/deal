a9cf2d35d165b1a7b30183b283dfdee4d9fd6d03
Giuseppe Viola <gius.viola@gmail.com>
2017-01-27 09:23:45 +0100

    expression

--- expression.lisp
+++ expression.lisp
@@ -4,8 +4,26 @@
 (defprod expression (attr ((exp string)))
   (to-list () `(attr (:exp ,exp))))
 
-(defprod expression (rel (())))
+(defprod expression (cat (&rest (exps expression)))
+  (to-list () `(cat (:exps ,(synth-all to-list exps)))))
 
+(defprod boolean-expression (<true> ())
+  (to-list () `(<true>)))
+(defprod boolean-expression (<false> ())
+  (to-list () `(<false>)))
+(defprod boolean-expression (<and> (&rest (exps boolean-expression)))
+  (to-list () `(<and> (:exps ,(synth-all to-list exps)))))
+(defprod boolean-expression (<or> (&rest (exps boolean-expression)))
+  (to-list () `(<or> (:exps ,(synth-all to-list exps)))))
+(defprod boolean-expression (<not> ((exp boolean-expression)))
+    (to-list () `(<not> (:exp ,(synth to-list exp)))))
+(defprod boolean-expression (<equal> ((exp1 expression) (exp2 expression)))
+    (to-list () `(<equal> (:exp1 ,(synth to-list exp1) ,(synth to-list exp2)))))
+(defprod boolean-expression (<less-than> ((exp1 expression) (exp2 expression)))
+    (to-list () `(<less-than> (:exp1 ,(synth to-list exp1) ,(synth to-list exp2)))))
+(defprod boolean-expression (<greater-than> ((exp1 expression) (exp2 expression)))
+    (to-list () `(<greater-than> (:exp1 ,(synth to-list exp1) ,(synth to-list exp2)))))
 ;; (defprods expression 
 ;;     (const (or string number))
 ;;   )
+
