6409ab79e542223999a7394ef8ecdc495a3c76ff
giusv <gius.viola@gmail.com>
2017-01-30 10:55:09 +0100

    work backup

--- gui.lisp
+++ gui.lisp
@@ -1,10 +1,12 @@
+(defprod transition (transition (url url))
+  (to-list () `(transition )))
 (defprod element (button ((id string) (expr expression)
 			  &optional (transition transition)))
-  (to-list () `(button (:id ,id :expr ,expr :transition ,(synth to-list transition)))))
+  (to-list () `(button (:id ,id :expr ,(synth to-list expr) :transition ,(synth to-list transition)))))
 
 (defprod element (input ((id string)
 			 &optional (expr expression)))
-  (to-list () `(input (:id ,id :expr ,expr))))
+  (to-list () `(input (:id ,id :expr ,(synth to-list expr)))))
 (defprod element (horz (&rest (elements (list element))))
   (to-list () `(horz (:elements ,(synth-all to-list elements)))))
 
@@ -12,7 +14,11 @@
   (to-list () `(vert (:elements ,(synth-all to-list elements)))))
 
 (defprod element (alt (&rest (elements (plist element))))
-  (to-list () `(alt (:elements ,(synth-all to-list elements)))))
+  (to-list () `(alt (:elements ,(synth-plist to-list elements)))))
 
 (alt :home (button 'ok (const "ok"))
      :login (input 'userid))
+
+(synth to-list (vert (button 'ok (const "ok")) (input 'userid)))
+
+
