cc6543e42a78f822c5ca7dcc2174706db86879a1
Giuseppe Viola <gius.viola@gmail.com>
2017-02-09 06:33:35 +0100

    attempt at form specification

---
+++ form.lisp
@@ -0,0 +1,11 @@
+(defparameter *form*
+ (let* ((user *user*)
+	(name (input 'name :bind (user >> name)))
+	(number (input 'number :bind (user >> numbers >> number)))
+	(numbers (replicate number :bind (user >> numbers)))
+	(city (input 'city :bind (user >> addresses >> address >> city)))
+	(state (input 'state :bind (user >> addresses >> address >> state)))
+	(addresses (replicate address :bind (user >> addresses)))
+	(address (vert city state) :bind (user >> addresses >> address))
+	(ok (button 'ok (const "Submit") :click ())))
+   (form user (vert name addresses numbers))))
