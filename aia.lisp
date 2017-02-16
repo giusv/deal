d6c231a19dd605092844490084c208c00b2449e6
Giuseppe Viola <gius.viola@gmail.com>
2017-02-16 06:27:57 +0100

    hub spoke

--- aia.lisp
+++ aia.lisp
@@ -36,6 +36,17 @@
 
 ;;(synth output (synth to-req *form* (void)) 0)
 
+(defun option-panel (label target)
+  (panel (label (const label))
+	 (anchor (gensym) (const "Vai alla pagina") :click (target target))))
+(defmacro hub-spoke (pairs base layout)
+  `(let* ,(mapcar #'(lambda (pair)
+		      (destructuring-bind (name label) pair
+			`(,name (option-panel ,label ,(url `(,base / ,name))))))
+		  pairs)
+     ,layout))
+
+
 (defparameter *aia* 
   (alt nil 
        (static :login nil  
@@ -45,10 +56,10 @@
 		      (cancel (button 'cancel (const "cancel"))))
 		 (vert userid passwd (horz ok cancel))))
        (static :home nil 
-	       (let* ((search-by-plate 
-		       (panel (label (const "Ricerca per targa"))
-			      (anchor (gensym) (const "Vai alla pagina") :click (target (void))))))
-		 (vert (horz search-by-plate))))))
+	       (hub-spoke ((search-by-plate "Ricerca per targa" )
+			   (search-by-person "Ricerca per persona"))
+			  home
+			  (horz search-by-plate search-by-person)))))
 
 (defparameter *gui-test* (let* ((search-by-plate 
 		       (panel (label (const "Ricerca per targa"))
@@ -63,4 +74,4 @@
 					     (meta (list :charset "utf-8"))
 					     (link (list :rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"))
 					     (link (list :rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css")))
-				       (body nil (synth to-html *gui-test* (void))))) 0))
+				       (body nil (synth to-html *aia* (void))))) 0))
